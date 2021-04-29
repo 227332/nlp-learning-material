a8bf7c6ba860e1edf9d8e91739a8b2322679171f
Rosa S <17686382+227332@users.noreply.github.com>
2021-04-29 22:25:37 +0200

    Add nlp helper script

--- text_summarization/summarizers/BaseSummarizer.py
+++ text_summarization/summarizers/BaseSummarizer.py
@@ -3,9 +3,13 @@ from abc import ABC, abstractmethod
 
 class BaseSummarizer(ABC):
 
-    def __init__(self, language: str):
-        self.language = language
+    def __init__(self, language: str) -> None:
+        self._language = language
+
+    @property
+    def language(self) -> str:
+        return self._language
 
     @abstractmethod
-    def summarize(self, text: str, summary_length: int):
+    def summarize(self, text: str, summary_length: int) -> List[str]:
         pass
--- text_summarization/summarizers/LSASummarizer.py
+++ text_summarization/summarizers/LSASummarizer.py
@@ -1,14 +1,10 @@
-import re
-import string
 from typing import List, Any
 
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
 import numpy as np
 from numpy.linalg import svd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+from helpers.NLPHelper import NLPHelper
 from summarizers.BaseSummarizer import BaseSummarizer
 
 
@@ -21,18 +17,17 @@ class LSASummarizer(BaseSummarizer):
 
     def __init__(self, language: str) -> None:
         super().__init__(language)
-        self._stemmer = SnowballStemmer(language)
-        self._stopwords = set(stopwords.words(language))
+        self._nlp_helper = NLPHelper(language)
 
     def summarize(self, text: str, summary_length: int = 3) -> List[str]:
         """Summarize input text with a desired number of sentences"""
-        sentences = sent_tokenize(text, language=self.language)
+        sentences = self._nlp_helper.get_sentences(text)
         text_length = len(sentences)
         if summary_length >= text_length or text_length==0:
             return sentences
         if summary_length <= 0:
             return []
-        preprocessed_sentences = [self._preprocess_text(sentence) for sentence in sentences]
+        preprocessed_sentences = [self._nlp_helper.preprocess_text(sentence) for sentence in sentences]
         term_sentence_matrix = self._compute_term_sentence_matrix(preprocessed_sentences)
         term_topic_matrix, sigma_vector, topic_sentence_matrix = singular_value_decomposition(term_sentence_matrix, full_matrices=False)
         sentence_scores = self._compute_squared_salience_scores(sigma_vector, topic_sentence_matrix)
@@ -43,39 +38,14 @@ class LSASummarizer(BaseSummarizer):
         top_sentence_indices.sort()
         return [sentences[index] for index in top_sentence_indices]
 
-    def _preprocess_text(self, text: str) -> str: 
-        """Pre-process input text with conversion to lowercase, punctuation removal,
-        stopwords removal and stemming
-        """
-        text_without_punctuation = self._remove_punctuation(text.lower())
-        tokens = word_tokenize(text_without_punctuation)
-        tokens_without_stopwords = self._remove_stopwords(tokens)
-        preprocessed_tokens = [self._stem_token(token) for token in tokens_without_stopwords]
-        preprocessed_text = ' '.join(preprocessed_tokens)
-        return preprocessed_text
-
-    def _remove_punctuation(self, text: str) -> str:
-        """Remove punctuation from input text"""
-        text_with_whitespace_replaced_by_space = re.sub("\s+", " ", text).strip()
-        punctuation_characters = string.punctuation + '’'  # ’ not ascii character
-        regex_pattern = "[{}]+".format(re.escape(punctuation_characters))
-        text_without_punctuation = re.sub(regex_pattern, '', text_with_whitespace_replaced_by_space)
-        return text_without_punctuation
-
-    def _remove_stopwords(self, tokens: List[str]) -> List[str]:
-        """Remove stopwords from a list of tokens"""
-        return [token for token in tokens if token not in self._stopwords]
-
-    def _stem_token(self, token: str) -> str:
-        """Perform stemming on a token"""
-        return self._stemmer.stem(token)
-
+    @staticmethod
     def _compute_term_sentence_matrix(self, sentences: List[str]) -> Any:
         """Compute the weighted term-sentence matrix"""
         vectorizer = TfidfVectorizer(ngram_range=(1, 1), stop_words=None)
         matrix = vectorizer.fit_transform(sentences)
         return matrix
 
+    @staticmethod
     def _compute_squared_salience_scores(self, sigma_vector: Any, topic_sentence_matrix: Any) -> Any:
         """Compute the squared value of the salience score of each sentence.
         The salience score is also called sentence length in the literature
